136f817f7ea7ad7c8a18fd66d13597e9db7c29d2
Wart-Hog <dblsergio@gmail.com>
2021-02-14 16:02:13 +0100

    qualcos'aptro

--- FrontEnd/src/app/components/home/home.component.ts
+++ FrontEnd/src/app/components/home/home.component.ts
@@ -11,17 +11,20 @@ export class HomeComponent implements OnInit {
     {
       name: "music",
       img: "https://dotravel.com/uploads/articles/96/live-music-barcelona-feature.jpg",
-      description: "See all music events"
+      description: "See all music events",
+      link: "http://localhost:4200/events/music"
     },
     {
       name: "theatre",
       img: "https://www.romatoday.it/~media/horizontal-hi/28653256639913/gigi-proietti-globe-theatre-2-2.jpg",
-      description: "See all theatre shows"
+      description: "See all theatre shows",
+      link: "http://localhost:4200/events/music"
     },
     {
       name: "sport",
       img: "https://montagneepaesi.com/wp-content/uploads/2020/05/sport-1024x683.jpg",
-      description: "See all sport events"
+      description: "See all sport events",
+      link: "http://localhost:4200/events/music"
     }
 
   ]
