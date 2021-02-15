2bf72ee3ea7b6077cfe8f8e1c36a891b54697eb1
CicciiSacket <55915430+CicciiSacket@users.noreply.github.com>
2021-02-15 18:03:34 +0100

    Separazione file test

--- BackEnd/src/test/testUser.ts
+++ BackEnd/src/test/testUser.ts
@@ -1,21 +1,20 @@
-import {expect} from 'chai';
 import request from 'supertest'
 const app = require('../app');
 
-describe("Get Test", () => {//OK
+describe("Get Test", () => {
     it('/users [200]', (done) => { 
         request(app).get('/users').expect(200,done);
     });
-    it('/users/:username [200]', (done) => {
+    it('/users/:username [200]', (done) => {//ok
         request(app).get('/users/pincpall').set("token", "LGB3G9e2tpq6PTFpj1Ssn5").expect(200,done);
     });
-    it('/users/:username [401]', (done) => {
+    it('/users/:username [401]', (done) => {//ok
         request(app).get('/users/pincpall').set("token", "null").expect(401,done);
     });
-    it('/users/:username/tickets [200]', (done) => {
+    it('/users/:username/tickets [200]', (done) => {//ok
         request(app).get('/users/pincpall').set("token", "LGB3G9e2tpq6PTFpj1Ssn5").expect(200,done);
     });
-    it('/users/:username/tickets [401]', (done) => {
+    it('/users/:username/tickets [401]', (done) => {//ok
         request(app).get('/users/pincpall').set("token", "null").expect(401,done);
     });
 });
