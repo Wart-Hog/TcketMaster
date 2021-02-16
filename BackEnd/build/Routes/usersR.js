c6966150109b66bc73891de8d3d585f1bd9450ce
Wart-Hog <dblsergio@gmail.com>
2021-02-16 16:19:23 +0100

    merge_dev1+dev2_OK

--- BackEnd/build/Routes/usersR.js
+++ BackEnd/build/Routes/usersR.js
@@ -34,7 +34,7 @@ exports.router.post('', function (_a, res) {
     users_list = users_list.concat(user);
     var new_users_list = JSON.stringify(users_list, null, 2);
     fs.writeFileSync('users_list.json', new_users_list);
-    res.json("created");
+    res.status(201).json({ message: "created" });
 });
 exports.router.post('/login', function (_a, res) {
     var _b = _a.body, username = _b.username, password = _b.password;
@@ -64,7 +64,7 @@ exports.router.post('/:username/tickets', middlewere_1.checkTokenHeader, functio
     users_list[userIndex].tickets.push(newticket);
     var new_users_list = JSON.stringify(users_list, null, 2);
     fs.writeFileSync('users_list.json', new_users_list);
-    res.json({ message: "ticket created" });
+    res.status(201).json({ message: "ticket created" });
 });
 exports.router.delete('/:username/tickets/:ticketId', middlewere_1.checkTokenHeader, function (_a, res) {
     var _b = _a.params, username = _b.username, ticketId = _b.ticketId;
@@ -86,12 +86,12 @@ exports.router.get('/:username/tickets', middlewere_1.checkTokenHeader, function
         return res.status(404).json({ message: "user not found" });
     res.json(users_list[usernameIndex].tickets);
 });
-exports.router.delete('/', middlewere_1.checkTokenHeader, function (_a, res) {
-    var username = _a.body.username;
-    var toDelete = users_list.findIndex(function (item) { return item.username == username; });
-    if (toDelete === -1)
+exports.router.delete('/:username', middlewere_1.checkTokenHeader, function (_a, res) {
+    var username = _a.params.username;
+    var toDeleted = users_list.findIndex(function (item) { return item.username == username; });
+    if (toDeleted == -1)
         return res.status(404).json({ message: "resource not found" });
-    users_list = users_list.splice(toDelete, 1);
+    users_list.splice(toDeleted, 1);
     var new_users_list = JSON.stringify(users_list, null, 2);
     fs.writeFileSync('users_list.json', new_users_list);
     res.status(201).json({ message: "resource deleted" });
--- BackEnd/build/test/testUser.js
+++ BackEnd/build/test/testUser.js
@@ -5,51 +5,48 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 var supertest_1 = __importDefault(require("supertest"));
 var app = require('../app');
-describe("Get Test", function () {
-    it('/users [200]', function (done) {
-        supertest_1.default(app).get('/users').expect(200, done);
-    });
-    it('/users/:username [200]', function (done) {
-        supertest_1.default(app).get('/users/pincpall').set("token", "C9ioCXoenoTTvWKyvBDKKD").expect(200, done);
-    });
-    it('/users/:username [401]', function (done) {
-        supertest_1.default(app).get('/users/pincpall').set("token", "null").expect(401, done);
-    });
-    it('/users/:username/tickets [200]', function (done) {
-        supertest_1.default(app).get('/users/pincpall').set("token", "C9ioCXoenoTTvWKyvBDKKD").expect(200, done);
-    });
-    it('/users/:username/tickets [401]', function (done) {
-        supertest_1.default(app).get('/users/pincpall').set("token", "null").expect(401, done);
-    });
-});
-// describe("POST test",()=>{
-//     it('users [201]',(done)=>{//ok
-//         request(app).post('/users').send({name:"pippo",username:"pluto",password:'pippo'}).expect(200,done);
+// describe("Get Test", () => {
+//     it('/users [200]', (done) => { 
+//         request(app).get('/users').expect(200,done);
 //     });
-//     it('users/login [200]',(done)=>{//ok
-//         request(app).post('/users/login').send({username:'pluto',password:'pippo'}).expect(200,done);
+//     it('/users/:username [200]', (done) => {//ok
+//         request(app).get('/users/pincpall').set("token", "TJV26ZueLNNd445eQRcWJP").expect(200,done);
 //     });
-//     it('users/login [404]',(done)=>{//ok
-//         request(app).post('/users/login').send({username:'pincpalla',password:'pincpall'}).expect(404,done);
+//     it('/users/:username [401]', (done) => {//ok
+//         request(app).get('/users/pincpall').set("token", "null").expect(401,done);
 //     });
 //     it('/users/:username/tickets [200]', (done) => {//ok
-//         request(app).post('/users/pincpall/tickets').set("token", "C9ioCXoenoTTvWKyvBDKKD").send({eventId:"4f6dfac1-31a5-4097-93ab-3d5d8e9bae19"}).expect(200,done);
+//         request(app).get('/users/pincpall').set("token", "TJV26ZueLNNd445eQRcWJP").expect(200,done);
 //     });
 //     it('/users/:username/tickets [401]', (done) => {//ok
-//         request(app).post('/users/pincpall/tickets').set("token", "null").send({eventId:"4f6dfac1-31a5-4097-93ab-3d5d8e9bae19"}).expect(401,done);
-//     });
-//     it('/users/:username/tickets [404]', (done) => {//ok
-//         request(app).post('/users/pincpall/tickets').set("token", "C9ioCXoenoTTvWKyvBDKKD").send({eventId:"null"}).expect(404,done);
+//         request(app).get('/users/pincpall').set("token", "null").expect(401,done);
 //     });
 // });
-// describe('DELETE test',()=>{
+describe("Post test", function () {
+    it('users [201]', function (done) {
+        supertest_1.default(app).post('/users').send({ name: "pippo", username: "pippo", password: 'pippo' }).expect(201, done);
+    });
+    it('users/login [200]', function (done) {
+        supertest_1.default(app).post('/users/login').send({ username: 'pippo', password: 'pippo' }).expect(200, done);
+    });
+    it('users/login [404]', function (done) {
+        supertest_1.default(app).post('/users/login').send({ username: 'pincpalla', password: 'pincpall' }).expect(404, done);
+    });
+    it('/users/:username/tickets [201]', function (done) {
+        supertest_1.default(app).post('/users/pincpall/tickets').set("token", "TJV26ZueLNNd445eQRcWJP").send({ eventId: "4f6dfac1-31a5-4097-93ab-3d5d8e9bae19" }).expect(201, done);
+    });
+    it('/users/:username/tickets [401]', function (done) {
+        supertest_1.default(app).post('/users/pincpall/tickets').set("token", "null").send({ eventId: "fc86c927-020b-446a-a329-049bd3b20395" }).expect(401, done);
+    });
+    it('/users/:username/tickets [404]', function (done) {
+        supertest_1.default(app).post('/users/pincpall/tickets').set("token", "TJV26ZueLNNd445eQRcWJP").send({ eventId: "null" }).expect(404, done);
+    });
+});
+// describe('Delete test',()=>{
 //     it('/users [201]',(done)=>{//ok
-//         request(app).delete('/users').set("token","Xo7MsuSZN91ET7wPoLZ8eJ").send({username:"pluto"}).expect(201,done)
+//         request(app).delete('/users/pluto').set("token","CBhArUkJP4nPu45oLCGfYT").expect(201,done)
 //     });
 //     it('/users [401]',(done)=>{//ok
-//         request(app).delete('/users').set("token","null").send({username:"pluto"}).expect(401,done)
-//     });
-//     it('/users [404]',(done)=>{//ok
-//         request(app).delete('/users').set("token","C9ioCXoenoTTvWKyvBDKKD").send({username:"mario"}).expect(404,done)
+//         request(app).delete('/users/pippo2').set("token","null").expect(401,done)
 //     });
 // });
