8315bea67edd90d9a54646af95e1ce91ebb01e5d
Wart-Hog <dblsergio@gmail.com>
2021-02-16 14:51:02 +0100

    Revert "Revert "Merge branch 'dev' into dev2""
    This reverts commit 70cb955d11320e99176408cdec4cc14866c16529.

--- FrontEnd/src/app/services/user.service.ts
+++ FrontEnd/src/app/services/user.service.ts
@@ -10,5 +10,22 @@ export class UserService {
   url = "http://localhost:3005/users"
   username = sessionStorage.getItem('username')
   constructor(private httpClient: HttpClient) { }
-  myTickets = ():Promise<ITicket[]> => this.httpClient.get(`${this.url}/${this.username}/tickets`).toPromise() as Promise<ITicket[]>
+  myTickets = ():Promise<ITicket[]> =>{
+    let headers = new HttpHeaders()
+    headers = headers.set('token',sessionStorage.getItem('token') || "")
+    return this.httpClient.get(`${this.url}/${this.username}/tickets`,{headers}).toPromise() as Promise<ITicket[]>
+  } 
+
+  buyTicket = ():Promise<any> => {
+    let headers = new HttpHeaders()
+    headers = headers.set('token',sessionStorage.getItem('token') || "")
+    return this.httpClient.post(`${this.url}/${this.username}/tickets`, {eventId: sessionStorage.getItem('ticket') },{headers}).toPromise() as Promise<any>
+  }
+  removeTicket = ():Promise<any> => {
+    let headers = new HttpHeaders()
+    headers = headers.set('token',sessionStorage.getItem('token') || "")
+    let ticketId = sessionStorage.getItem('ticketID')
+    console.log(ticketId)
+    return this.httpClient.delete(`${this.url}/${this.username}/tickets/${ticketId}`,{headers}).toPromise() as Promise<any>
+  }
 }
